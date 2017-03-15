a5a56910f9e4e924be8b228484a1c27ba7af48bb
sumitha <sumithas2@yahoo.com>
2017-03-15 13:50:01 -0400

    modified erb_example.rb

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,10 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+
+firstName = 'Sumitha'
+lastName = 'Binu'
+
+template = ERB.new ("My Name is <%= firstName + ' ' + lastName %>")
+puts template.result(binding)
